0535a09b8d11680fc982d0ab13f3784550ce370c
Daniil Zao <idanix@list.ru>
2024-02-23 04:28:49 +0300

    Cleanup listener

--- bukkit/src/main/java/ru/overwrite/protect/bukkit/listeners/AdditionalListener.java
+++ bukkit/src/main/java/ru/overwrite/protect/bukkit/listeners/AdditionalListener.java
@@ -1,96 +1,65 @@
 package ru.overwrite.protect.bukkit.listeners;
 
 import com.destroystokyo.paper.event.server.AsyncTabCompleteEvent;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import ru.overwrite.protect.bukkit.ServerProtectorManager;
 import ru.overwrite.protect.bukkit.api.ServerProtectorAPI;
 import ru.overwrite.protect.bukkit.utils.Config;
 
 public class AdditionalListener implements Listener {
-
-	private final ServerProtectorManager instance;
 	private final ServerProtectorAPI api;
 	private final Config pluginConfig;
 
 	public AdditionalListener(ServerProtectorManager plugin) {
-		instance = plugin;
 		api = plugin.getPluginAPI();
 		pluginConfig = plugin.getPluginConfig();
 	}
 
 	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
 	public void onItemDrop(PlayerDropItemEvent e) {
-		if (instance.login.isEmpty())
-			return;
-		Player p = e.getPlayer();
-		if (pluginConfig.blocking_settings_block_item_drop) {
-			api.handleInteraction(p, e);
-		}
+		handleEvent(e.getPlayer(), e, pluginConfig.blocking_settings_block_item_drop);
 	}
 
 	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
 	public void onItemPickup(EntityPickupItemEvent e) {
-		if (instance.login.isEmpty())
-			return;
-		if (!(e.getEntity() instanceof Player))
-			return;
-		Player p = (Player) e.getEntity();
-		if (pluginConfig.blocking_settings_block_item_pickup) {
-			api.handleInteraction(p, e);
-		}
+		handleEvent(e.getEntity(), e, pluginConfig.blocking_settings_block_item_pickup);
 	}
 
 	@EventHandler(ignoreCancelled = true)
 	public void onTabComplete(AsyncTabCompleteEvent e) {
-		if (instance.login.isEmpty())
-			return;
-		if (!(e.getSender() instanceof Player))
-			return;
-		Player p = (Player) e.getSender();
-		if (pluginConfig.blocking_settings_block_tab_complete) {
-			api.handleInteraction(p, e);
-		}
+		handleEvent(e.getSender(), e, pluginConfig.blocking_settings_block_item_pickup);
 	}
 
 	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
 	public void onPlayerDamage(EntityDamageEvent e) {
-		if (instance.login.isEmpty())
-			return;
-		if (!(e.getEntity() instanceof Player))
-			return;
-		Player p = (Player) e.getEntity();
-		if (pluginConfig.blocking_settings_block_damage) {
-			api.handleInteraction(p, e);
-		}
+		handleEvent(e.getEntity(), e, pluginConfig.blocking_settings_block_item_pickup);
 	}
 
 	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
 	public void onPlayerDamageEntity(EntityDamageByEntityEvent e) {
-		if (instance.login.isEmpty())
-			return;
-		if (!(e.getDamager() instanceof Player))
-			return;
-		Player p = (Player) e.getDamager();
-		if (pluginConfig.blocking_settings_damaging_entity) {
-			api.handleInteraction(p, e);
-		}
+		handleEvent(e.getDamager(), e, pluginConfig.blocking_settings_block_item_pickup);
 	}
 
 	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
 	public void onInventoryOpen(InventoryOpenEvent e) {
-		if (instance.login.isEmpty())
-			return;
-		Player p = (Player) e.getPlayer();
-		if (pluginConfig.blocking_settings_block_inventory_open) {
-			api.handleInteraction(p, e);
-		}
+		handleEvent(e.getPlayer(), e, pluginConfig.blocking_settings_block_item_pickup);
+	}
+
+	private void handleEvent(CommandSender sender, Cancellable event, boolean blocking) {
+		if (sender instanceof Player) handleEvent((Player) sender, event, blocking);
+	}
+
+	private void handleEvent(Player player, Cancellable event, boolean blocking) {
+		if (blocking) api.handleInteraction(player, event);
 	}
 }
